6faded9d3126b5befdb00a6edb82b89e087cfca6
Tigerenashe Matanda <tigerenashe.matanda@ecocashholdings.co.zw>
2022-02-27 20:06:03 +0200

    After creating models for the app

--- voters_registration/models.py
+++ voters_registration/models.py
@@ -1,3 +1,13 @@
+from ctypes import addressof
 from django.db import models
+from datetime import datetime,date
 
 # Create your models here.
+
+class Registration(models.Model):
+    firstname = models.CharField(max_length=15)
+    surname = models.CharField(max_length= 15)
+    id_number = models.CharField(max_length=13)
+    address = models.TextField(max_length=50)
+    date_of_birth = models.DateField(auto_now_add=False,auto_now=False,blank=True,null=True)
+    timestamp = models.DateField(auto_now_add=True,auto_now=False,blank=True,null=True)
